c69dff9e0eb4ee0268bf4b16c5a91eff98486ce9
Utku Utkan <utku.utkan@hoydaa.com>
2008-04-12 05:54:52 +0000

    New Forgot Password E-mail with dummy content.

---
+++ apps/frontend/modules/mail/templates/forgotPasswordSuccess.php
@@ -0,0 +1,18 @@
+<p>Dear askeet user,</p>
+
+<p>A request for <?php echo $mail->getSubject() ?> was sent to this address.</p>
+
+<p>For safety reasons, the askeet website does not store passwords in clear. When you forget your password, askeet creates a new one that can be used in place.</p>
+
+<p>You can now connect to your askeet profile with:</p>
+
+<p>
+	username: <strong><?php echo $username ?></strong><br/>
+	password: <strong><?php echo $password ?></strong>
+</p>
+
+<p>To get connected, go to the <a href="http://www.askeet.com/login">http://www.askeet.com/login</a> and enter these codes.</p>
+
+<p>We hope to see you soon on</p>
+
+<p>The askeet email robot</p>
