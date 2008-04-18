c700b4aa309a16625987648adfdf3f824fb82daf
Umut Utkan <umut.utkan@gmail.com>
2008-04-18 17:40:40 +0000

    changes in order to display error as well as message

--- apps/frontend/modules/site/templates/messageSuccess.php
+++ apps/frontend/modules/site/templates/messageSuccess.php
@@ -1,1 +1,5 @@
-<p class="message"><?php echo $sf_flash->get('message') ?></p>
+<?php if($sf_flash->get('message')): ?>
+	<p class="message"><?php echo $sf_flash->get('message') ?></p>
+<?php else: ?>
+	<p class="error"><?php echo $sf_flash->get('error') ?></p>
+<?php endif; ?>
