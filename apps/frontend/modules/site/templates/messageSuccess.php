db41e153e187a18c8b76d9e77388ca96f0949bca
Umut Utkan <umut.utkan@gmail.com>
2008-04-18 17:42:36 +0000

    message->info

--- apps/frontend/modules/site/templates/messageSuccess.php
+++ apps/frontend/modules/site/templates/messageSuccess.php
@@ -1,5 +1,5 @@
-<?php if($sf_flash->get('message')): ?>
-	<p class="message"><?php echo $sf_flash->get('message') ?></p>
+<?php if($sf_flash->get('info')): ?>
+	<p class="info"><?php echo $sf_flash->get('info') ?></p>
 <?php else: ?>
 	<p class="error"><?php echo $sf_flash->get('error') ?></p>
 <?php endif; ?>
