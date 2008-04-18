835a3180bacc9aba87ec53aa1bfce7c95c891b0d
Umut Utkan <umut.utkan@gmail.com>
2008-04-18 17:41:49 +0000

    *** empty log message ***

--- apps/frontend/modules/code/templates/_createdBy.php
+++ apps/frontend/modules/code/templates/_createdBy.php
@@ -2,5 +2,5 @@
 
 <?php echo ' created by ' . 
     link_to($code->getContributor(), 'sfLucene/search?query=contributor:' . $code->getContributor()) . 
-	' on ' . format_datetime($code->getCreatedAt()) . 
+	' on ' . format_date($code->getCreatedAt()) . 
 	' with tags: ' . link_to_tags($code->getTags()) ?>
