51454dd6a1a93fdc6c2d3369ebddb790fd3f1956
Umut Utkan <umut.utkan@gmail.com>
2008-04-18 18:19:24 +0000

    deleted, not being used anymore.

--- apps/frontend/modules/sidebar/templates/_default.php
+++
@@ -1,5 +0,0 @@
-<?php if($sf_user->isAuthenticated()): ?>
-	<?php include_component('sidebar', 'user') ?>
-<?php endif; ?>
-<?php include_component('sidebar', 'most') ?>
-<?php include_component('sidebar', 'tags') ?>
