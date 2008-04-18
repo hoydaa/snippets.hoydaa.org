db61fe6d0e216fa4bd917d1b52d58ff668cf5dc1
Utku Utkan <utku.utkan@hoydaa.com>
2008-04-18 18:06:51 +0000

    Variable names are changed.

--- apps/frontend/modules/mail/templates/registerSuccess.altbody.php
+++ apps/frontend/modules/mail/templates/registerSuccess.altbody.php
@@ -1,6 +1,6 @@
 Confirm your email address!
-Dear <?php echo $user->getProfile()->getFullName() ?>,
+Dear <?php echo $full_name ?>,
 Click the link below to verify your e-mail address and activate your account.
-<?php echo $_SERVER['SERVER_NAME'] . '/user/confirmation?key=' . $user->getProfile()->getConfirmation() ?>
+<?php echo $_SERVER['SERVER_NAME'] . '/user/confirmation?key=' . $activation_key ?>
 Thanks for signing up.
 The Hoydaa Codesnippets Team
--- apps/frontend/modules/mail/templates/registerSuccess.php
+++ apps/frontend/modules/mail/templates/registerSuccess.php
@@ -1,7 +1,7 @@
 <p>Confirm your email address!</p>
-<p>Dear <?php echo $user->getProfile()->getFullName() ?>,</p>
+<p>Dear <?php echo $full_name ?>,</p>
 <p>Click the link below to verify your e-mail address and activate your account.</p>
 <p><?php echo link_to('Verify my account', 
-	'user/confirmation?key=' . $user->getProfile()->getConfirmation(), array('absolute' => true)) ?></p>
+	'user/confirmation?key=' . $activation_key, array('absolute' => true)) ?></p>
 <p>Thanks for signing up.</p>
 <p>The Hoydaa Codesnippets Team</p>
