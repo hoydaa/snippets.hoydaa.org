2918da3e4e0cc7b7f132979a57437d615454fd90
Umut Utkan <umut.utkan@gmail.com>
2008-07-08 20:05:01 +0000

    SNIPPETS-5: the expression tagged as is replaced with tagged with

--- apps/frontend/modules/tag/templates/showSuccess.php
+++ apps/frontend/modules/tag/templates/showSuccess.php
@@ -1,5 +1,5 @@
 <?php use_helper('I18N') ?>
 
-<h1><?php echo __('Snippets tagged as %1%.', array('%1%' => link_to($sf_params->get('tag'), 'tag/show?tag='.$sf_params->get('tag')))) ?></h1>
+<h1><?php echo __('Snippets tagged with %1%.', array('%1%' => link_to($sf_params->get('tag'), 'tag/show?tag='.$sf_params->get('tag')))) ?></h1>
 
 <?php include_partial('snippet/list', array('pager' => $pager)) ?>
