a2cd3d5040e7e6c0910f59658b224f3e12226691
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-10 21:38:06 +0000

    Minimize and maximize icons are added.

--- apps/frontend/modules/tag/templates/_tagCloud.php
+++ apps/frontend/modules/tag/templates/_tagCloud.php
@@ -1,15 +1,18 @@
 <?php use_helper('Javascript') ?>
 <div class="sidebox">
-	<div class="bottom">
-		<div class="content">
-			<?php echo link_to_blind('tag_cloud_box', image_tag('tags.gif')) ?>
-		</div>
-		<div class="content" id="tag_cloud_box">
-			<ul id="tag_cloud">
-			    <?php foreach($tags as $tag => $rank): ?>
-			    <li class="rank_<?php echo $rank ?>"><?php echo link_to($tag, 'tag/show?tag=' . $tag) ?></li>
-			    <?php endforeach; ?>
-			</ul>
-		</div>
-	</div>
+    <div class="bottom">
+        <div class="content" id="tag_cloud_box">
+            <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td><?php echo image_tag('tags.gif') ?></td>
+                    <td align="right"><?php echo toggle('tag_cloud', 'tag_cloud-up', 'tag_cloud-down') ?></td>
+                </tr>
+            </table>
+            <ul id="tag_cloud">
+                <?php foreach($tags as $tag => $rank): ?>
+                <li class="rank_<?php echo $rank ?>"><?php echo link_to($tag, 'tag/show?tag=' . $tag) ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
 </div>
