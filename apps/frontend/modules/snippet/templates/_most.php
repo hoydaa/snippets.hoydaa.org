d0b62dc56288d5a3ea7ce8e21a24519b9e9b6414
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-10 21:37:53 +0000

    Minimize and maximize icons are added.

--- apps/frontend/modules/snippet/templates/_most.php
+++ apps/frontend/modules/snippet/templates/_most.php
@@ -1,39 +1,44 @@
 <?php use_helper('Date', 'I18N', 'My', 'Javascript') ?>
 <div class="sidebox">
-	<div class="bottom">
-		<div class="content">
-			<?php echo link_to_blind('most_box', image_tag('snippets.gif')) ?>
-		</div>
-		<div class="content" id="most_box">
-			<ol>
-				<?php foreach ($snippets as $snippet): ?>
-				<li>
-					<h3><?php echo link_to($snippet->getTitle(), 'snippet/show?id='.$snippet->getId(), 'class=title'); ?></h3>
-					<?php include_partial('snippet/postedBy', array('code' => $snippet)) ?>
-				</li>
-				<?php endforeach; ?>
-			</ol>
-			<?php echo __('Display') ?>:
-			<?php echo select_tag(
-				'most',
-				options_for_select(
-					array(
-						'new' => 'New Snippets',
-						'high' => 'Highest Rated Snippets',
-						'disc' => 'Most Discussed Snippets'
-					),
-					$sf_params->get('most')
-				),
-				array(
-					'onchange' => remote_function(
-						array(
-							'update' => 'sidebar-snippets',
-							'url' => 'snippet/most',
-							'with' => '"most=" + value',
-						)
-					),
-				)
-			) ?>
+    <div class="bottom">
+        <div class="content" id="most_box">
+            <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td><?php echo image_tag('snippets.gif') ?></td>
+                    <td align="right"><?php echo toggle('snippets-hide', 'snippets-up', 'snippets-down') ?></td>
+                </tr>
+            </table>
+            <div id="snippets-hide">
+                <ol>
+                    <?php foreach ($snippets as $snippet): ?>
+                    <li>
+                        <h3><?php echo link_to($snippet->getTitle(), 'snippet/show?id='.$snippet->getId(), 'class=title'); ?></h3>
+                        <?php include_partial('snippet/postedBy', array('code' => $snippet)) ?>
+                    </li>
+                    <?php endforeach; ?>
+                </ol>
+                <?php echo __('Display') ?>:
+                <?php echo select_tag(
+                    'most',
+                    options_for_select(
+                        array(
+                            'new' => 'New Snippets',
+                            'high' => 'Highest Rated Snippets',
+                            'disc' => 'Most Discussed Snippets'
+                        ),
+                        $sf_params->get('most')
+                    ),
+                    array(
+                        'onchange' => remote_function(
+                            array(
+                                'update' => 'sidebar-snippets',
+                                'url' => 'snippet/most',
+                                'with' => '"most=" + value',
+                            )
+                        ),
+                    )
+                ) ?>
+            </div>
 		</div>
 	</div>
 </div>
