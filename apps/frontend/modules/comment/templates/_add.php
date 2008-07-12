27808be50ba6ecde33f80793244a02fecf4d3734
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-12 13:38:30 +0000

    SNIPPETS-24: Required fields are marked with an asterisk.

--- apps/frontend/modules/comment/templates/_add.php
+++ apps/frontend/modules/comment/templates/_add.php
@@ -1,4 +1,4 @@
-<?php use_helper('I18N', 'Validation', 'Javascript', 'Cryptographp') ?>
+<?php use_helper('I18N', 'Validation', 'Javascript', 'Cryptographp', 'My') ?>
 
 <?php echo form_remote_tag(array(
   'url' => 'comment/add',
@@ -8,18 +8,18 @@
   <?php echo input_hidden_tag('id', $sf_params->get('id')) ?>
   <?php if(!$sf_user->isAuthenticated()): ?>
   <div class="row">
-    <?php echo label_for('name', __('Name')) ?>
+    <?php echo label_for('name', __('Name') . required()) ?>
     <?php echo input_tag('name', $sf_params->get('name')) ?>
     <?php echo form_error('name') ?>
   </div>
   <div class="row">
-    <?php echo label_for('email', __('Email')) ?>
+    <?php echo label_for('email', __('Email') . required()) ?>
     <?php echo input_tag('email', $sf_params->get('email')) ?>
     <?php echo form_error('email') ?>
   </div>
   <?php endif; ?>
   <div class="row">
-    <?php echo label_for('body', __('Comment')) ?>
+    <?php echo label_for('body', __('Comment') . required()) ?>
     <?php echo textarea_tag('body', $sf_params->get('body')) ?>
     <?php echo form_error('body') ?>            
   </div>
@@ -27,7 +27,7 @@
     <?php echo cryptographp_picture() ?><?php echo cryptographp_reload() ?>
   </div>
   <div class="row">
-    <?php echo label_for('captcha', __('Type the code shown')) ?>
+    <?php echo label_for('captcha', __('Type the code shown') . required()) ?>
     <?php echo input_tag('captcha', $sf_params->get('captcha')) ?>
     <?php echo form_error('captcha') ?>
   </div>
