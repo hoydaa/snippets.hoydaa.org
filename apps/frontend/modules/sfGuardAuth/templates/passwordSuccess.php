3e99ccc1a3457c1d7a6b313c808e3439dc1e42a3
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-12 13:38:41 +0000

    SNIPPETS-24: Required fields are marked with an asterisk.

--- apps/frontend/modules/sfGuardAuth/templates/passwordSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/passwordSuccess.php
@@ -1,8 +1,8 @@
-<?php use_helper('I18N', 'Validation') ?>
+<?php use_helper('I18N', 'Validation', 'My') ?>
 
 <?php echo form_tag('sfGuardAuth/password') ?>
 	<div class="row">
-		<?php echo label_for('email', __('Email')) ?>
+		<?php echo label_for('email', __('Email') . required()) ?>
 		<?php echo input_tag('email', $sf_params->get('email')) ?>
 		<?php echo form_error('email') ?>
 	</div>        
