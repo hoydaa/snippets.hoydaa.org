706cc450421021197f1b6fd502fa5a40ee66a7a5
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-16 15:29:33 +0000

    SNIPPETS-34: Index action is added to serve static content.

--- apps/frontend/modules/site/actions/actions.class.php
+++ apps/frontend/modules/site/actions/actions.class.php
@@ -1,8 +1,22 @@
 <?php
 
-class siteActions extends sfActions {
+class siteActions extends sfActions
+{
+  public function executeIndex()
+  {
+    $this->content = $this->getRequestParameter('content');
 
-	public function executeMessage() {
-	}
+    $this->forward404Unless($this->partialExists($this->getContext(), $this->content));
+  }
 
+  public function executeMessage()
+  {
+  }
+
+  protected function partialExists($context, $name)
+  {
+    $directory = $context->getModuleDirectory();
+
+    return is_readable($directory . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . '_' . $name . '.php');
+  }
 }
