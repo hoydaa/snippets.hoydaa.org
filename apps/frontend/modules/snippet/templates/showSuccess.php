8cffc2fe7f0dadf804dae30e80fb6c9b7372bff4
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-16 08:03:37 +0000

    SNIPPETS-32: Delete icon is added.

--- apps/frontend/modules/snippet/templates/showSuccess.php
+++ apps/frontend/modules/snippet/templates/showSuccess.php
@@ -2,7 +2,7 @@
 
 <h1><?php echo $code->getTitle() ?></h1>
 <?php if($sf_user->isAuthenticated() && myUtils::isUserRecord('SnippetPeer', $code->getId(), $sf_user->getGuardUser()->getId())): ?>
-<?php echo link_to(image_tag('page_edit.png', array ('alt' => __('Edit'), 'title' => __('Edit'))), 'snippet/edit?id=' . $code->getId()) ?>
+<?php echo link_to(image_tag('page_edit.png', array ('alt' => __('Edit'), 'title' => __('Edit'))), 'snippet/edit?id=' . $code->getId()) ?> <?php echo link_to(image_tag('page_delete.png', array ('alt' => __('Delete'), 'title' => __('Delete'))), 'snippet/delete?id=' . $code->getId(), array ('confirm' => 'Are you sure you want to delete this snippet?')) ?>
 <?php endif; ?>
 <p><?php echo simple_format_text($code->getBody()) ?></p>
 <?php include_partial('snippet/postedBy', array('code' => $code)) ?>
