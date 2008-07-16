11959f24f47fe2dd75b44f9e81235152c59659c2
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-16 15:30:54 +0000

    SNIPPETS-34: Not used anymore.

--- apps/frontend/modules/site/templates/_message.php
+++
@@ -1,5 +0,0 @@
-<?php if ($sf_flash->get('info')): ?>
-<p class="info"><?php echo $sf_flash->get('info') ?></p>
-<?php elseif ($sf_flash->get('error')): ?>
-<p class="error"><?php echo $sf_flash->get('error') ?></p>
-<?php endif; ?>
