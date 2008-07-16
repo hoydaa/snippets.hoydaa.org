7fe4cefb429cf8fe217030c313c754ed1714f369
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-16 15:37:14 +0000

    SNIPPETS-34: Markdown helper is used to convert markdown-formatted text into HTML.

--- apps/frontend/modules/snippet/templates/showSuccess.php
+++ apps/frontend/modules/snippet/templates/showSuccess.php
@@ -1,10 +1,10 @@
-<?php use_helper('I18N', 'Text', 'sfRating') ?>
+<?php use_helper('I18N', 'Markdown', 'sfRating') ?>
 
 <h1><?php echo $code->getTitle() ?></h1>
 <?php if($sf_user->isAuthenticated() && myUtils::isUserRecord('SnippetPeer', $code->getId(), $sf_user->getGuardUser()->getId())): ?>
 <?php echo link_to(image_tag('page_edit.png', array ('alt' => __('Edit'), 'title' => __('Edit'))), 'snippet/edit?id=' . $code->getId()) ?> <?php echo link_to(image_tag('page_delete.png', array ('alt' => __('Delete'), 'title' => __('Delete'))), 'snippet/delete?id=' . $code->getId(), array ('confirm' => 'Are you sure you want to delete this snippet?')) ?>
 <?php endif; ?>
-<p><?php echo simple_format_text($code->getBody()) ?></p>
+<?php echo convert_markdown_text($code->getBody()) ?>
 <?php include_partial('snippet/postedBy', array('code' => $code)) ?>
 <br /><br />
 <?php echo sf_rater($code) ?>
