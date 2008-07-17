b5e81f3f5aeaed18f14c2e7f2e003d0c44c8878d
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-17 07:42:56 +0000

    SNIPPETS-38: Managed Content icon is added.

--- apps/frontend/modules/snippet/templates/showSuccess.php
+++ apps/frontend/modules/snippet/templates/showSuccess.php
@@ -1,8 +1,12 @@
 <?php use_helper('I18N', 'Markdown', 'sfRating') ?>
 
-<h1><?php echo $code->getTitle() ?></h1>
-<?php if($sf_user->isAuthenticated() && myUtils::isUserRecord('SnippetPeer', $code->getId(), $sf_user->getGuardUser()->getId())): ?>
-<?php echo link_to(image_tag('page_edit.png', array ('alt' => __('Edit'), 'title' => __('Edit'))), 'snippet/edit?id=' . $code->getId()) ?> <?php echo link_to(image_tag('page_delete.png', array ('alt' => __('Delete'), 'title' => __('Delete'))), 'snippet/delete?id=' . $code->getId(), array ('confirm' => 'Are you sure you want to delete this snippet?')) ?>
+<h1 class="title"><?php echo $code->getTitle() ?></h1>
+<?php if ($code->getMC() == 'true'): ?>
+<?php echo image_tag('flag_blue.png', array('alt' => __('Managed Content'), 'title' => __('Managed Content'))) ?> 
+<?php endif; ?>
+<?php if ($sf_user->isAuthenticated() && myUtils::isUserRecord('SnippetPeer', $code->getId(), $sf_user->getGuardUser()->getId())): ?>
+<?php echo link_to(image_tag('page_edit.png', array('alt' => __('Edit'), 'title' => __('Edit'))), 'snippet/edit?id=' . $code->getId()) ?> 
+<?php echo link_to(image_tag('page_delete.png', array ('alt' => __('Delete'), 'title' => __('Delete'))), 'snippet/delete?id=' . $code->getId(), array('confirm' => 'Are you sure you want to delete this snippet?')) ?>
 <?php endif; ?>
 <?php echo convert_markdown_text($code->getBody()) ?>
 <?php include_partial('snippet/postedBy', array('code' => $code)) ?>
