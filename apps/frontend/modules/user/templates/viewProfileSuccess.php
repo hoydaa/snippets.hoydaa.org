34acc69d5fde696e1126b74d74422d7cb9a6702d
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-17 08:16:10 +0000

    SNIPPETS-39: Editor icon is added.

--- apps/frontend/modules/user/templates/viewProfileSuccess.php
+++ apps/frontend/modules/user/templates/viewProfileSuccess.php
@@ -1,6 +1,9 @@
 <?php use_helper('I18N', 'Date') ?>
 
-<h1><?php echo $user->getUsername() . __('\'s profile') ?></h1>
+<h1 class="title"><?php echo $user->getUsername() . __('\'s profile') ?></h1>
+<?php if ($user->hasGroup('EDITOR')): ?>
+<?php echo image_tag('shield.png', array('alt' => __('Editor'), 'title' => __('Editor'))) ?>
+<?php endif; ?>
 
 <p><?php echo __('Posted %1% snippet(s) since %2%.', array('%1%' => $snippet_count, '%2%' => format_date($user->getCreatedAt()))) ?></p>
 
