73c23863d778bc82e18abd57d843b0099743144d
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-17 06:25:27 +0000

    SNIPPETS-37: Posted by expression is revised to fix the problem.

--- apps/frontend/modules/snippet/templates/_postedBy.php
+++ apps/frontend/modules/snippet/templates/_postedBy.php
@@ -4,20 +4,41 @@
 
 $params = array();
 
+// Contributor parameter is set.
 if ($code->getSfGuardUserId())
+{
   $params['%contributor%'] = link_to($code->getContributor(), 'user/viewProfile?username='.$code->getContributor());
+}
 else
+{
   $params['%contributor%'] = $code->getContributor();
+}
 
+// Date parameter is set.
 $params['%date%'] = format_date($code->getCreatedAt());
 
+// Tags parameter is set.
+$tag_links = array();
 $tags = explode(', ', $code->getTag());
 
 for ($i = 0; $i < count($tags); $i++)
-  $tags[$i] = link_to($tags[$i], 'tag/show?tag='.$tags[$i]);
+{
+  if ($tags[$i])
+  {
+    $tag_links[$i] = link_to($tags[$i], 'tag/show?tag='.$tags[$i]);
+  }
+}
 
-$params['%tags%'] = implode(', ', $tags);
+$params['%tags%'] = implode(', ', $tag_links);
+
+// Posted by expression is printed.
+if ($params['%tags%'])
+{
+  echo __('posted by %contributor% on %date% with tags %tags%', $params);
+}
+else
+{
+  echo __('posted by %contributor% on %date%', $params);
+}
 
 ?>
-
-<?php echo __('posted by %contributor% on %date% with tags %tags%', $params) ?>
