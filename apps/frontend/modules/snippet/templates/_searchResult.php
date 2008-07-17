4673f2b0750d3f5bd0dc773ea5037f1896805991
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-17 07:42:45 +0000

    SNIPPETS-38: Managed Content icon is added.

--- apps/frontend/modules/snippet/templates/_searchResult.php
+++ apps/frontend/modules/snippet/templates/_searchResult.php
@@ -1,3 +1,6 @@
-<h2><?php echo link_to($result->getTitle(), 'snippet/show?id='.$result->getId()); ?></h2>
+<h2 class="title"><?php echo link_to($result->getTitle(), 'snippet/show?id='.$result->getId()); ?></h2>
+<?php if ($result->getMC() == 'true'): ?>
+<?php echo image_tag('flag_blue.png', array('alt' => __('Managed Content'), 'title' => __('Managed Content'))) ?> 
+<?php endif; ?>
 <p><?php echo $result->getDescription() ?></p>
 <?php include_partial('snippet/postedBy', array('code' => $result)) ?>
