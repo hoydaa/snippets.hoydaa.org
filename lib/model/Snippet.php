1070399e9b81e7a8743a89637b6e6baaf3a463b4
Umut Utkan <umut.utkan@gmail.com>
2008-07-18 20:16:34 +0000

    Code cleanup.

--- lib/model/Snippet.php
+++ lib/model/Snippet.php
@@ -37,27 +37,6 @@ class Snippet extends BaseSnippet
     else
       return 'false';
   }
-
-    public function save($con = null) {
-        $salt = $this->getTitle().$this->getDescription();
-        $snippet_languages = Snippet::getSnippetLanguagess($this->getBody());
-        foreach($this->getSnippetLanguages() as $language) {
-            $language->delete();
-        }
-        foreach($snippet_languages as $language) {
-            $code_language = new SnippetLanguage();
-            $code_language->setSnippet($this);
-            $code_language->setLanguage($language);
-            $this->addSnippetLanguage($code_language);
-        }
-        parent::save($con);
-    }
-    
-    public function getHtmlizeCode() {
-        $soap = new SoapClient("http://localhost:8080/axis2/services/CodesnippetService?wsdl");
-        $rtn = $soap->highlight(array("language"=>$this->getCodeLanguage()->getTag(), "code"=>$this->getCode()));
-        return $rtn->return;
-    }
     
     public function getComments($criteria = null, $con = null) {
         $c = new Criteria();
@@ -78,20 +57,6 @@ class Snippet extends BaseSnippet
     public function getContributor() {
         return ($this->getSfGuardUserId() ? 
             $this->getSfGuardUser()->getUsername() : $this->getName());
-    }
-    
-    public static function getSnippetLanguagess($snippet_code) {
-        $rtn_language = array();
-        $languages = LanguagePeer::doSelect(new Criteria());
-        foreach($languages as $language) {
-            $arr = array();
-            $exp = sprintf(self::$REG_EXPRESSION, $language->getTag(), $language->getTag());
-            preg_match_all($exp, $snippet_code, $arr, PREG_SET_ORDER);
-//            preg_match_all("/<".$language->getTag()."\-snippet>(.+)<\/".$language->getTag()."\-snippet>/isU", $snippet_code, $arr, PREG_SET_ORDER); 
-            if(sizeof($arr) > 0)
-                $rtn_language[] = $language;
-        }
-        return $rtn_language;
     }
 }
 
