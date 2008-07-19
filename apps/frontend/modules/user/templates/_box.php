3a83e1b9e0fd3d285e26c6b10805bb79401d8599
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-19 15:53:56 +0000

    SNIPPETS-50: Link that directs the user to My Comments page is fixed.

--- apps/frontend/modules/user/templates/_box.php
+++ apps/frontend/modules/user/templates/_box.php
@@ -12,7 +12,7 @@
       <div id="user-hide">
         <?php echo __('Welcome') . ' ' . link_to($sf_user->getGuardUser()->getUsername(), 'user/viewProfile?username=' . $sf_user->getGuardUser()->getUsername()) ?>!<br /><br />
         You have <?php echo link_to($user_code_count, 'snippet/listMySnippets') ?> snippet(s).<br /><br />
-        You have <?php echo link_to($user_comment_count, 'snippet/list?job=comment') ?> comment(s).<br /><br />
+        You have <?php echo link_to($user_comment_count, 'comment/listMine') ?> comment(s).<br /><br />
         <?php echo link_to(__('Account settings'), 'user/viewAccount') ?>
       </div>
     </div>
