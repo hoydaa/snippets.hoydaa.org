098a160dfc23088a1fb3135f8dba1ee283fb86e2
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-19 15:52:47 +0000

    SNIPPETS-50: Posted by expression is now generated inside the posted by partial.

--- apps/frontend/modules/comment/templates/_show.php
+++ apps/frontend/modules/comment/templates/_show.php
@@ -1,17 +1,2 @@
-<?php use_helper('I18N', 'Date', 'Markdown') ?>
-
-<?php
-
-$params = array();
-
-if ($comment->getSfGuardUserId())
-  $params['%posted_by%'] = link_to($comment->getSfGuardUser()->getUsername(), 'user/viewProfile?username='.$comment->getSfGuardUser()->getUsername());
-else
-  $params['%posted_by%'] = $comment->getName();
-
-$params['%date%'] = format_date($comment->getCreatedAt());
-
-?>
-
-<?php echo __('%posted_by% on %date%', $params) ?>
+<?php include_partial('comment/posted_by', array('comment' => $comment)) ?>
 <?php echo $comment->getBody() ?>
