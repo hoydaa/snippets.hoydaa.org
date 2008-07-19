f46841f4a53103c69cba62e9ed9b7ac37fecb12e
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-19 15:57:08 +0000

    SNIPPETS-50: Summary field is set.

--- lib/model/Comment.php
+++ lib/model/Comment.php
@@ -5,6 +5,7 @@ class Comment extends BaseComment
   public function save($con = null)
   {
     $this->setBody(sfMarkdown::doConvert($this->getRawBody()));
+    $this->setSummary(myUtils::extractSummary($this->getBody(), 10, 200));
 
     parent::save();
   }
