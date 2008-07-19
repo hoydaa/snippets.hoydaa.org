dfa5ff1e5776b88a72b87be724c5ee9ca961ce8b
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-19 15:51:24 +0000

    SNIPPETS-50: Partial for the comment's posted by expression.

---
+++ apps/frontend/modules/comment/templates/_posted_by.php
@@ -0,0 +1,16 @@
+<?php use_helper('I18N', 'Date', 'Markdown') ?>
+
+<?php
+
+$params = array();
+
+if ($comment->getSfGuardUserId())
+  $params['%posted_by%'] = link_to($comment->getSfGuardUser()->getUsername(), 'user/viewProfile?username='.$comment->getSfGuardUser()->getUsername());
+else
+  $params['%posted_by%'] = $comment->getName();
+
+$params['%date%'] = format_date($comment->getCreatedAt());
+
+?>
+
+<?php echo __('%posted_by% on %date%', $params) ?>
