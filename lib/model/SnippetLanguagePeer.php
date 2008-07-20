c5917c53afd9c60711bf1f853f8ec7fcd79c4861
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-20 08:38:19 +0000

    SNIPPETS-51: Method to return the popular programming languages is added.

--- lib/model/SnippetLanguagePeer.php
+++ lib/model/SnippetLanguagePeer.php
@@ -2,10 +2,45 @@
  
 class SnippetLanguagePeer extends BaseSnippetLanguagePeer
 {
-  public static function retrieveByTag($tag)
+  public static function getPopularValidLanguages($max = 10)
   {
-    $c = new Criteria();
-    $c->add(CodeLanguagePeer::TAG, $tag);
-    return CodeLanguagePeer::doSelectOne($c);
+    $connection = Propel::getConnection();
+
+    $languages = array_keys(sfConfig::get('app_languages'));
+
+    foreach ($languages as $i => $language)
+    {
+      $languages[$i] = "'$languages[$i]'";
+    }
+
+    $query = 'SELECT %s as language, COUNT(*) as count FROM %s WHERE %s IN (%s) GROUP BY language ORDER BY count DESC';
+    $query = sprintf($query, SnippetLanguagePeer::NAME, SnippetLanguagePeer::TABLE_NAME, SnippetLanguagePeer::NAME, implode(', ', $languages));
+
+    $statement = $connection->prepareStatement($query);
+
+    if ($max)
+    {
+      $statement->setLimit($max);
+    }
+
+    $resultset = $statement->executeQuery();
+
+    $languages = array();
+
+    $max_count = 0;
+
+    while($resultset->next())
+    {
+      if (!$max_count)
+      {
+        $max_count = $resultset->getInt('count');
+      }
+
+      $languages[$resultset->getString('language')] = floor(($resultset->getInt('count') / $max_count * 9) + 1);
+    }
+
+    ksort($languages);
+
+    return $languages;
   }
 }
