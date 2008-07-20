b8951590a0cb39831e2464ada53efe5ab323e64e
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-20 10:28:23 +0000

    An alternative summarizer.

---
+++ lib/Summarizer.class.php
@@ -0,0 +1,61 @@
+<?php
+
+sfLoader::loadHelpers(array('Text'));
+
+class Summarizer
+{
+  private $max;
+  private $threshold;
+
+  function __construct($max, $threshold = 10)
+  {
+    $this->max = $max;
+    $this->threshold = $threshold;
+  }
+
+  public function summarize($body)
+  {
+    $matches = array();
+    preg_match_all("/<p>(.+)<\/p>/isU", $body, $matches, PREG_SET_ORDER);
+
+    $paragraphs = array();
+
+    foreach ($matches as $match)
+    {
+      $paragraphs[] = preg_replace("/(\\r?\\n[ \\t]*)+/", " ", strip_tags($match[1]));
+    }
+
+    $total = $this->total($paragraphs);
+
+    foreach ($paragraphs as $i => $paragraph)
+    {
+      if (strlen($paragraph) / $total * 100 < $this->threshold)
+      {
+        unset($paragraphs[$i]);
+      }
+    }
+
+    $total = $this->total($paragraphs);
+
+    foreach ($paragraphs as $i => $paragraph)
+    {
+      $paragraphs[$i] = truncate_text($paragraph, strlen($paragraph) / $total * $this->max);
+    }
+
+    return implode(' ', $paragraphs);
+  }
+
+  protected function total($paragraphs)
+  {
+    $total = 0;
+
+    foreach ($paragraphs as $paragraph)
+    {
+      $total += strlen($paragraph);
+    }
+
+    return $total;
+  }
+}
+
+?>
