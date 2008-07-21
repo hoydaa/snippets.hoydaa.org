6483965e4e64c7bbdf6a18dc7ad271585712dc91
Umut Utkan <umut.utkan@gmail.com>
2008-07-21 17:01:36 +0000

    SNIPPETS-11: Preference system changes.

--- apps/frontend/modules/user/templates/_box.php
+++ apps/frontend/modules/user/templates/_box.php
@@ -6,10 +6,12 @@
       <table width="100%" border="0" cellpadding="0" cellspacing="0">
         <tr>
           <td><?php echo image_tag('user.gif') ?></td>
-          <td align="right"><?php echo toggle('user-hide', 'user-up', 'user-down') ?></td>
+          <td align="right"><?php echo toggle('user-hide', 'user-up', 'user-down',
+                  $sf_user->getPreference('box_user') == 'none' ? 'down' : 'up',
+                  remote_function(array('url' => "@set_preference?pname=box_user", 'with' => "&quot;pvalue=&quot; + $('user-up').style.display"))); ?></td>
         </tr>
       </table>
-      <div id="user-hide">
+      <div id="user-hide"<?php echo $sf_user->getPreference('box_user') == 'none' ? " style=display:none;" : "" ?>>
         <?php echo __('Welcome') . ' ' . link_to($sf_user->getGuardUser()->getUsername(), 'user/viewProfile?username=' . $sf_user->getGuardUser()->getUsername()) ?>!<br /><br />
         You have <?php echo link_to($user_code_count, 'snippet/listMySnippets') ?> snippet(s).<br /><br />
         You have <?php echo link_to($user_comment_count, 'comment/listMine') ?> comment(s).<br /><br />
