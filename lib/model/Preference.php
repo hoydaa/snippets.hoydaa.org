d92df2328dac2aa98240b6792f01cc67240d6c15
Umut Utkan <umut.utkan@gmail.com>
2008-07-21 19:02:54 +0000

    SNIPPETS-11: Table neeeded for preference system is added.

---
+++ lib/model/Preference.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for representing a row from the 'snippets_preference' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class Preference extends BasePreference
+{
+}
---
+++ lib/model/PreferencePeer.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Subclass for performing query and update operations on the 'snippets_preference' table.
+ *
+ * 
+ *
+ * @package lib.model
+ */ 
+class PreferencePeer extends BasePreferencePeer
+{
+}
