82ad25c7d692e86bd91464f7355e62e12c03ad74
Umut Utkan <umut.utkan@gmail.com>
2008-07-21 17:00:58 +0000

    SNIPPETS-11: Preference system changes.

--- apps/frontend/modules/tag/templates/_tagCloud.php
+++ apps/frontend/modules/tag/templates/_tagCloud.php
@@ -5,10 +5,12 @@
             <table width="100%" border="0" cellpadding="0" cellspacing="0">
                 <tr>
                     <td><?php echo image_tag('tags.gif') ?></td>
-                    <td align="right"><?php echo toggle('tags-hide', 'tag_cloud-up', 'tag_cloud-down') ?></td>
+                    <td align="right"><?php echo toggle('tags-hide', 'tag_cloud-up', 'tag_cloud-down',
+                    		$sf_user->getPreference('box_tag_cloud') == 'none' ? 'down' : 'up',
+                            remote_function(array('url' => "@set_preference?pname=box_tag_cloud", 'with' => "&quot;pvalue=&quot; + $('tag_cloud-up').style.display"))); ?></td>
                 </tr>
             </table>
-            <div id="tags-hide">
+            <div id="tags-hide"<?php echo $sf_user->getPreference('box_tag_cloud') == 'none' ? " style=display:none;" : "" ?>>
                 <ul class="tag_cloud few_tags">
                     <?php foreach($tags as $tag => $rank): ?>
                     <li class="rank_<?php echo $rank ?>"><?php echo link_to($tag, 'tag/show?tag=' . $tag) ?></li>
