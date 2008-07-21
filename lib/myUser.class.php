03a5be3ef4eb5c8af058e94adfcb5ed7f6c45332
Umut Utkan <umut.utkan@gmail.com>
2008-07-21 17:02:19 +0000

    SNIPPETS-11: Preference system changes.

--- lib/myUser.class.php
+++ lib/myUser.class.php
@@ -7,4 +7,20 @@ class myUser extends sfGuardSecurityUser
         return $this->getGuardUser()->getId();
     }
     
+    public function setPreference($preference, $value)
+    {
+		$this->setAttribute("app_preference_$preference", $value);
+    }
+    
+    public function getPreference($preference)
+    {
+    	if(($item = $this->getAttribute("app_preference_$preference"))) 
+    	{
+    		return $item;
+    	}
+    	$item = sfConfig::get("app_preference_$preference");
+    	sfLogger::getInstance()->info("Sending default preference for app_preference_$preference=$item");
+    	return $item;
+    }
+    
 }
