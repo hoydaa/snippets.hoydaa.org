ce9ddf98fe7f99c3c72e6526e3b2cdd5ec8df5bb
Umut Utkan <umut.utkan@gmail.com>
2008-07-21 17:00:32 +0000

    SNIPPETS-11: Preference system changes.

--- apps/frontend/modules/snippet/templates/_most.php
+++ apps/frontend/modules/snippet/templates/_most.php
@@ -1,14 +1,27 @@
 <?php use_helper('Date', 'I18N', 'My', 'Javascript') ?>
+<script language="javascript">
+    function update_most() {
+        <?php echo remote_function(
+            array(
+                'update' => 'sidebar-snippets',
+                'url' => 'snippet/most',
+                'with' => "\"most=\" + $('most').value"
+            )
+        ) ?>    
+    }
+</script>
 <div class="sidebox">
     <div class="bottom">
         <div class="content" id="most_box">
             <table width="100%" border="0" cellpadding="0" cellspacing="0">
                 <tr>
                     <td><?php echo image_tag('snippets.gif') ?></td>
-                    <td align="right"><?php echo toggle('snippets-hide', 'snippets-up', 'snippets-down') ?></td>
+                    <td align="right"><?php echo toggle('snippets-hide', 'snippets-up', 'snippets-down', 
+                    		$sf_user->getPreference('box_snippets') == 'none' ? 'down' : 'up',
+                    		remote_function(array('url' => "@set_preference?pname=box_snippets", 'with' => "&quot;pvalue=&quot; + $('snippets-up').style.display"))); ?></td>
                 </tr>
             </table>
-            <div id="snippets-hide">
+            <div id="snippets-hide"<?php echo $sf_user->getPreference('box_snippets') == 'none' ? " style=display:none;" : "" ?>>
                 <ol>
                     <?php foreach ($snippets as $snippet): ?>
                     <li>
@@ -21,7 +34,7 @@
                     </li>
                     <?php endforeach; ?>
                 </ol>
-                <?php echo __('Display') ?>:
+                <?php echo __('Show') ?>:
                 <?php echo select_tag(
                     'most',
                     options_for_select(
@@ -33,14 +46,24 @@
                         $sf_params->get('most')
                     ),
                     array(
-                        'onchange' => remote_function(
-                            array(
-                                'update' => 'sidebar-snippets',
-                                'url' => 'snippet/most',
-                                'with' => '"most=" + value',
-                            )
-                        ),
+                        'onchange' => "update_most();"
                     )
+                ) ?>
+                <?php echo select_tag(
+                    'most_size',
+                    options_for_select(
+                        array(
+                            '5' => '5',
+                            '10' => '10',
+                            '15' => '15',
+                            '20' => '20',
+                            '25' => '25'
+                        ),
+                        $sf_user->getPreference('box_snippets_size')
+                    ),
+                    array(
+                        'onchange' => remote_function(array('url' => "@set_preference?pname=box_snippets_size", 'with' => "&quot;pvalue=&quot; + value", 'success' => 'update_most();'))
+                    )                    
                 ) ?>
             </div>
 		</div>
