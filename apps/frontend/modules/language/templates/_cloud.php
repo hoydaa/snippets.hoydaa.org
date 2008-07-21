42570fe6d093739d1647eecc2d428963e243e2a1
Umut Utkan <umut.utkan@gmail.com>
2008-07-21 16:58:42 +0000

    SNIPPETS-11: Preference system changes.

--- apps/frontend/modules/language/templates/_cloud.php
+++ apps/frontend/modules/language/templates/_cloud.php
@@ -5,10 +5,12 @@
             <table width="100%" border="0" cellpadding="0" cellspacing="0">
                 <tr>
                     <td><?php echo image_tag('languages.gif') ?></td>
-                    <td align="right"><?php echo toggle('languages-hide', 'language_cloud-up', 'language_cloud-down') ?></td>
+                    <td align="right"><?php echo toggle('languages-hide', 'language_cloud-up', 'language_cloud-down',
+                            $sf_user->getPreference('box_language_cloud') == 'none' ? 'down' : 'up',
+                            remote_function(array('url' => "@set_preference?pname=box_language_cloud", 'with' => "&quot;pvalue=&quot; + $('language_cloud-up').style.display"))); ?></td>
                 </tr>
             </table>
-            <div id="languages-hide">
+            <div id="languages-hide"<?php echo $sf_user->getPreference('box_language_cloud') == 'none' ? " style=display:none;" : "" ?>>
                 <ul class="tag_cloud few_tags">
                     <?php foreach($languages as $language => $rank): ?>
                     <li class="rank_<?php echo $rank ?>"><?php echo link_to($language, 'language/show?language=' . $language) ?></li>
