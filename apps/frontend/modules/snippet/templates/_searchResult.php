a3eced571703bbbf62c02511f4141ac602d9b730
Umut Utkan <umut.utkan@gmail.com>
2008-07-23 17:05:42 +0000

    General corrections.

--- apps/frontend/modules/snippet/templates/_searchResult.php
+++ apps/frontend/modules/snippet/templates/_searchResult.php
@@ -1,4 +1,5 @@
 <?php use_helper('My', 'Date') ?>
+
 <h2 class="title"><?php echo link_to($result->getTitle(), 'snippet/show?id='.$result->getId()); ?></h2>
 <?php if ($result->getMC() == 'true'): ?>
 <?php echo image_tag('flag_blue.png', array('alt' => __('Managed Content'), 'title' => __('Managed Content'))) ?>
