159817313141765a006bf03407c5116b31dd674e
Umut Utkan <umut.utkan@gmail.com>
2008-07-23 20:12:19 +0000

    SNIPPETS-57: Titles are added.

--- apps/frontend/modules/language/templates/_cloud.php
+++ apps/frontend/modules/language/templates/_cloud.php
@@ -12,8 +12,8 @@
             </table>
             <div id="languages-hide"<?php echo $sf_user->getPreference('box_language_cloud') == 'none' ? " style=display:none;" : "" ?>>
                 <ul class="tag_cloud few_tags">
-                    <?php foreach($languages as $language => $rank): ?>
-                    <li class="rank_<?php echo $rank ?>"><?php echo link_to($language, 'language/show?language=' . $language) ?></li>
+                    <?php foreach($languages as $language): ?>
+                    <li class="rank_<?php echo $language['rank'] ?>"><?php echo link_to($language['language'], 'language/show?language=' . $language['language'], array('title' => __(":count snippet(s) for ':language'", array(':count' => $language['count'], ':language' => $language['language'])))) ?></li>
                     <?php endforeach; ?>
                 </ul>
                 <?php echo link_to(__('View All'), 'language/list') ?>
