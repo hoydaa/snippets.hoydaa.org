2fbe7245fe0a4c0853718187f35bb46fb01b2e0f
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-23 06:18:40 +0000

    Pager navigation is moved to the snippet module's list partial.

--- apps/frontend/modules/snippet/templates/listMySnippetsSuccess.php
+++ apps/frontend/modules/snippet/templates/listMySnippetsSuccess.php
@@ -3,23 +3,3 @@
 <h1><?php echo __('Your Snippets') ?></h1>
 
 <?php include_partial('snippet/list', array('pager' => $pager)) ?>
-
-<?php if ($pager->haveToPaginate()): ?>
-<ul class="search-page-numbers">
-    <?php if ($pager->getPage() != $pager->getPreviousPage()): ?>
-    <li><?php echo link_to(__('Prev'), 'snippet/listMySnippets?page=' . $pager->getPreviousPage()) ?></li>
-    <?php endif; ?>
-    <?php foreach ($pager->getLinks() as $page): ?>
-    <li>
-        <?php if ($page == $pager->getPage()): ?>
-        <span><?php echo $page ?></span>
-        <?php else: ?>
-        <?php echo link_to($page, 'snippet/listMySnippets?page=' . $page) ?>
-        <?php endif; ?>
-    </li>
-    <?php endforeach; ?>
-    <?php if ($pager->getPage() != $pager->getNextPage()): ?>
-    <li><?php echo link_to(__('Next'), 'snippet/listMySnippets?page=' . $pager->getNextPage()) ?></li>
-    <?php endif; ?>
-</ul>
-<?php endif; ?>
