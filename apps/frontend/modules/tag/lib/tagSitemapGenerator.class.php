b3730b5d0f0e9d9c32f6b66008b693b6aeaa1ed8
Umut Utkan <umut.utkan@gmail.com>
2008-07-23 20:12:42 +0000

    SNIPPETS-57: In order to support changes.

--- apps/frontend/modules/tag/lib/tagSitemapGenerator.class.php
+++ apps/frontend/modules/tag/lib/tagSitemapGenerator.class.php
@@ -5,8 +5,8 @@ class tagSitemapGenerator implements sitemapGenerator
     	$urls = array();
     	$tags = TagPeer::getPopularTags(500);
     	$urls[] = new sitemapURL("tag/list", date('F j Y'));
-    	foreach($tags as $tag => $rank) {
-    		$urls[] = new sitemapURL("tag/".$tag, date('F j, Y'));
+    	foreach($tags as $tag) {
+    		$urls[] = new sitemapURL("tag/".$tag['tag'], date('F j, Y'));
     	}
     	return $urls;
     }
