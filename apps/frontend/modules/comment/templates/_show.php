09154c8d07c17b705d79e390eeaa2bdc942b5444
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-23 07:30:02 +0000

    Posted by expression is now generated via MyHelper.

--- apps/frontend/modules/comment/templates/_show.php
+++ apps/frontend/modules/comment/templates/_show.php
@@ -1,2 +1,4 @@
-<?php include_partial('comment/posted_by', array('comment' => $comment)) ?>
+<?php use_helper('My') ?>
+
+<?php echo comment_posted_by($comment) ?>
 <?php echo $comment->getBody() ?>
