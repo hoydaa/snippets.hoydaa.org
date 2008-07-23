e48f5bfc76cb939b7532f8d77f686963a31d75f0
Umut Utkan <umut.utkan@gmail.com>
2008-07-23 20:13:01 +0000

    SNIPPETS-57: Titles are added.

--- apps/frontend/modules/tag/templates/listSuccess.php
+++ apps/frontend/modules/tag/templates/listSuccess.php
@@ -3,7 +3,7 @@
 <h1><?php echo __('All Tags') ?></h1>
 
 <ul class="tag_cloud all_tags">
-  <?php foreach($tags as $tag => $rank): ?>
-    <li class="rank_<?php echo $rank ?>"><?php echo link_to($tag, 'tag/show?tag=' . $tag) ?></li>
+  <?php foreach($tags as $tag): ?>
+    <li class="rank_<?php echo $tag['rank'] ?>"><?php echo link_to($tag['tag'], 'tag/show?tag=' . $tag['tag'], array('title' => __(":count snippet(s) are tagged with ':tag'", array(':count' => $tag['count'], ':tag' => $tag['tag'])))) ?></li>
   <?php endforeach; ?>
 </ul>
