94fa13f52ea5f01530a183a548ee72b4768a692e
Umut Utkan <umut.utkan@gmail.com>
2008-07-23 21:31:09 +0000

    SNIPPETS-57: Titles are added.

--- apps/frontend/modules/tag/templates/_tagCloud.php
+++ apps/frontend/modules/tag/templates/_tagCloud.php
@@ -13,7 +13,7 @@
             <div id="tags-hide"<?php echo $sf_user->getPreference('box_tag_cloud') == 'none' ? " style=display:none;" : "" ?>>
                 <ul class="tag_cloud few_tags">
                     <?php foreach($tags as $tag): ?>
-                    <li class="rank_<?php echo $tag['rank'] ?>"><?php echo link_to($tag['tag'], 'tag/show?tag=' . $tag['tag'], array('title' => __(":count snippet(s) are tagged with ':tag'", array(':count' => $tag['count'], ':tag' => $tag['tag'])))) ?></li>
+                    <li class="rank_<?php echo $tag['rank'] ?>"><?php echo link_to($tag['tag'], 'tag/show?tag=' . $tag['tag'], array('title' => __(":count snippet(s) with tag ':tag'", array(':count' => $tag['count'], ':tag' => $tag['tag'])))) ?></li>
                     <?php endforeach; ?>
                 </ul>
                 <?php echo link_to(__('View All'), 'tag/list') ?>
