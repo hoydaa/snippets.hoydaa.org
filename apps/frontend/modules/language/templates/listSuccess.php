7532f098d53342d19aad800f66cf919caf8a314f
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-24 12:41:51 +0000

    Title attributes of language links are changed.

--- apps/frontend/modules/language/templates/listSuccess.php
+++ apps/frontend/modules/language/templates/listSuccess.php
@@ -4,6 +4,6 @@
 
 <ul class="tag_cloud all_tags">
   <?php foreach($languages as $language): ?>
-    <li class="rank_<?php echo $language['rank'] ?>"><?php echo link_to($language['language'], 'language/show?language=' . $language['language'], array('title' => __(":count snippet(s) for ':language'", array(':count' => $language['count'], ':language' => $language['language'])))) ?></li>
+    <li class="rank_<?php echo $language['rank'] ?>"><?php echo link_to($language['language'], 'language/show?language=' . $language['language'], array('title' => __(":count snippet(s) with ':language' code", array(':count' => $language['count'], ':language' => $language['language'])))) ?></li>
   <?php endforeach; ?>
 </ul>
