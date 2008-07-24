1c3b648704b16eba9b664709809c67a31ec8eeb5
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-24 12:41:38 +0000

    Title attributes of language links are changed.

--- apps/frontend/modules/language/templates/_cloud.php
+++ apps/frontend/modules/language/templates/_cloud.php
@@ -13,7 +13,7 @@
             <div id="languages-hide"<?php echo $sf_user->getPreference('box_language_cloud') == 'none' ? " style=display:none;" : "" ?>>
                 <ul class="tag_cloud few_tags">
                     <?php foreach($languages as $language): ?>
-                    <li class="rank_<?php echo $language['rank'] ?>"><?php echo link_to($language['language'], 'language/show?language=' . $language['language'], array('title' => __(":count snippet(s) for ':language'", array(':count' => $language['count'], ':language' => $language['language'])))) ?></li>
+                    <li class="rank_<?php echo $language['rank'] ?>"><?php echo link_to($language['language'], 'language/show?language=' . $language['language'], array('title' => __(":count snippet(s) with ':language' code", array(':count' => $language['count'], ':language' => $language['language'])))) ?></li>
                     <?php endforeach; ?>
                 </ul>
                 <?php echo link_to(__('View All'), 'language/list') ?>
