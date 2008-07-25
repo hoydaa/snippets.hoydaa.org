dc88a4e5edb768c672293dc121d8cd8df279fbf1
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-25 13:13:03 +0000

    Some design changes are made.

--- apps/frontend/modules/site/templates/indexSuccess.php
+++ apps/frontend/modules/site/templates/indexSuccess.php
@@ -1,11 +1,7 @@
 <?php echo image_tag('banner.gif') ?>
 <br /><br />
-<ol class="search-results">
-    <?php foreach ($snippets as $snippet): ?>
-    <li>
-        <h1 class="title"><?php echo link_to($snippet->getTitle(), 'snippet/show?id=' . $snippet->getId()); ?></h1>
-
-        <?php include_partial('snippet/show', array('code' => $snippet)) ?>
-    </li>
-    <?php endforeach ?>
-</ol>
+<?php foreach ($snippets as $snippet): ?>
+<h1 class="title"><?php echo link_to($snippet->getTitle(), 'snippet/show?id=' . $snippet->getId()); ?></h1>
+<?php include_partial('snippet/show', array('code' => $snippet)) ?>
+<div class="hr"></div>
+<?php endforeach ?>
