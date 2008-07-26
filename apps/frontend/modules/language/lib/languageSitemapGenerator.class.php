47885f66182069faec3b503f9894677b541ce0fa
Umut Utkan <umut.utkan@gmail.com>
2008-07-26 13:25:54 +0000

    SNIPPETS-63: Fixed.

--- apps/frontend/modules/language/lib/languageSitemapGenerator.class.php
+++ apps/frontend/modules/language/lib/languageSitemapGenerator.class.php
@@ -4,8 +4,8 @@ class languageSitemapGenerator implements sitemapGenerator
     public static function generate() {
     	$urls = array();
     	$langs = SnippetLanguagePeer::getPopularValidLanguages(500);
-    	foreach($langs as $lang => $rank) {
-    		$urls[] = new sitemapURL("language/".$lang, date('F j, Y'));
+    	foreach($langs as $lang) {
+    		$urls[] = new sitemapURL("language/".$lang['language'], date('F j, Y'));
     	}
     	return $urls;
     }
