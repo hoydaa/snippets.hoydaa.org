35699056c28dd16691dc76667c1f9a334e63951d
Umut Utkan <umut.utkan@gmail.com>
2008-07-26 08:04:10 +0000

    Correction.

--- apps/frontend/modules/snippet/templates/_most.php
+++ apps/frontend/modules/snippet/templates/_most.php
@@ -25,7 +25,7 @@
                 <ol>
                     <?php foreach ($snippets as $snippet): ?>
                     <li>
-                        <h3 class="title"><?php echo link_to($snippet->getTitle(), 'snippet/show?id='.$snippet->getId(), 'class=title'); ?></h3>
+                        <h3 class="title"><?php echo link_to($snippet->getTitle(), 'snippet/show?id='.$snippet->getId(), array('class' => 'title')); ?></h3>
                         <?php if ($snippet->getMC() == 'true'): ?>
                         <?php echo image_tag('flag_blue.png', array('alt' => __('Managed Content'), 'title' => __('Managed Content'))) ?>
                         <?php endif; ?>
