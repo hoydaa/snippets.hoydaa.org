7bf74177b107170725697f7a8f83a6c3a909ee4b
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-27 13:25:09 +0000

    Template is simplified.

--- apps/frontend/modules/tag/templates/_tagCloud.php
+++ apps/frontend/modules/tag/templates/_tagCloud.php
@@ -2,14 +2,10 @@
 <div class="sidebox">
     <div class="bottom">
         <div class="content" id="tag_cloud_box">
-            <table width="100%" border="0" cellpadding="0" cellspacing="0">
-                <tr>
-                    <td><?php echo image_tag('tags.gif') ?></td>
-                    <td align="right"><?php echo toggle('tags-hide', 'tag_cloud-up', 'tag_cloud-down',
-                    		$sf_user->getPreference('box_tag_cloud') == 'none' ? 'down' : 'up',
-                            remote_function(array('url' => "@set_preference?pname=box_tag_cloud", 'with' => "&quot;pvalue=&quot; + $('tag_cloud-up').style.display"))); ?></td>
-                </tr>
-            </table>
+            <?php echo toggle('tags-hide', 'tag_cloud-up', 'tag_cloud-down',
+                    $sf_user->getPreference('box_tag_cloud') == 'none' ? 'down' : 'up',
+                    remote_function(array('url' => "@set_preference?pname=box_tag_cloud", 'with' => "&quot;pvalue=&quot; + $('tag_cloud-up').style.display"))); ?>
+            <?php echo image_tag('tags.gif') ?>
             <div id="tags-hide"<?php echo $sf_user->getPreference('box_tag_cloud') == 'none' ? " style=display:none;" : "" ?>>
                 <ul class="tag_cloud few_tags">
                     <?php foreach($tags as $tag): ?>
