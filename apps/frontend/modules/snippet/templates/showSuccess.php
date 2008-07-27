fc42c59b440251a75c84ee46de8015ade0934ad9
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-27 14:45:01 +0000

    Snippet title is removed from the snippet partial named 'show'.

--- apps/frontend/modules/snippet/templates/showSuccess.php
+++ apps/frontend/modules/snippet/templates/showSuccess.php
@@ -1,3 +1,18 @@
+<?php use_helper('I18N') ?>
+
+<h1>
+    <span><?php echo $code->getTitle() ?></span>
+    <?php if ($code->getMC() == 'true'): ?>
+    <?php echo image_tag('flag_blue.png', array('alt' => __('Managed Content'), 'title' => __('Managed Content'))) ?>
+    <?php endif; ?>
+
+    <?php if ($sf_user->isAuthenticated() && myUtils::isUserRecord('SnippetPeer', $code->getId(), $sf_user->getGuardUser()->getId())): ?>
+    <?php echo link_to(image_tag('page_edit.png', array('alt' => __('Edit'), 'title' => __('Edit'))), 'snippet/edit?id=' . $code->getId()) ?>
+
+    <?php echo link_to(image_tag('page_delete.png', array ('alt' => __('Delete'), 'title' => __('Delete'))), 'snippet/delete?id=' . $code->getId(), array('confirm' => 'Are you sure you want to delete this snippet?')) ?>
+    <?php endif; ?>
+</h1>
+
 <?php include_partial('snippet/show', array('code' => $code)) ?>
 
 <?php include_partial('comment/list', array('comments' => $code->getComments())) ?>
