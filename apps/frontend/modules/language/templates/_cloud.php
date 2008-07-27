88e6ee1fa6adc95a7dd12bafec1f77c26a286898
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-27 13:24:45 +0000

    Template is simplified.

--- apps/frontend/modules/language/templates/_cloud.php
+++ apps/frontend/modules/language/templates/_cloud.php
@@ -2,14 +2,10 @@
 <div class="sidebox">
     <div class="bottom">
         <div class="content">
-            <table width="100%" border="0" cellpadding="0" cellspacing="0">
-                <tr>
-                    <td><?php echo image_tag('languages.gif') ?></td>
-                    <td align="right"><?php echo toggle('languages-hide', 'language_cloud-up', 'language_cloud-down',
-                            $sf_user->getPreference('box_language_cloud') == 'none' ? 'down' : 'up',
-                            remote_function(array('url' => "@set_preference?pname=box_language_cloud", 'with' => "&quot;pvalue=&quot; + $('language_cloud-up').style.display"))); ?></td>
-                </tr>
-            </table>
+            <?php echo toggle('languages-hide', 'language_cloud-up', 'language_cloud-down',
+                    $sf_user->getPreference('box_language_cloud') == 'none' ? 'down' : 'up',
+                    remote_function(array('url' => "@set_preference?pname=box_language_cloud", 'with' => "&quot;pvalue=&quot; + $('language_cloud-up').style.display"))); ?>
+            <?php echo image_tag('languages.gif') ?>
             <div id="languages-hide"<?php echo $sf_user->getPreference('box_language_cloud') == 'none' ? " style=display:none;" : "" ?>>
                 <ul class="tag_cloud few_tags">
                     <?php foreach($languages as $language): ?>
