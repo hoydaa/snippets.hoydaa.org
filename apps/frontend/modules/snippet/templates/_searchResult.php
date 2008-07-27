4a46d75b61a6db78e86cff6dcd3fc22aefd6671b
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-27 15:35:04 +0000

    Template layout is modified.

--- apps/frontend/modules/snippet/templates/_searchResult.php
+++ apps/frontend/modules/snippet/templates/_searchResult.php
@@ -1,8 +1,10 @@
 <?php use_helper('I18N', 'My') ?>
 
-<h2 class="title"><?php echo link_to($result->getTitle(), 'snippet/show?id='.$result->getId()); ?></h2>
-<?php if ($result->getMC() == 'true'): ?>
-<?php echo image_tag('flag_blue.png', array('alt' => __('Managed Content'), 'title' => __('Managed Content'))) ?>
-<?php endif; ?>
+<h2>
+    <span><?php echo link_to($result->getTitle(), 'snippet/show?id='.$result->getId()); ?></span>
+    <?php if ($result->getMC() == 'true'): ?>
+    <?php echo image_tag('flag_blue.png', array('alt' => __('Managed Content'), 'title' => __('Managed Content'))) ?>
+    <?php endif; ?>
+</h2>
 <p><?php echo $result->getSummary() ?></p>
-<?php echo snippet_posted_by($result) ?>
+<p><?php echo snippet_posted_by($result) ?></p>
