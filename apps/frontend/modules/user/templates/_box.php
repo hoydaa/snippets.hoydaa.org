4e4becf8a041b838ae81e9559e8c0204d266195c
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-27 13:25:20 +0000

    Template is simplified.

--- apps/frontend/modules/user/templates/_box.php
+++ apps/frontend/modules/user/templates/_box.php
@@ -3,14 +3,10 @@
 <div class="sidebox">
   <div class="bottom">
     <div class="content">
-      <table width="100%" border="0" cellpadding="0" cellspacing="0">
-        <tr>
-          <td><?php echo image_tag('user.gif') ?></td>
-          <td align="right"><?php echo toggle('user-hide', 'user-up', 'user-down',
-                  $sf_user->getPreference('box_user') == 'none' ? 'down' : 'up',
-                  remote_function(array('url' => "@set_preference?pname=box_user", 'with' => "&quot;pvalue=&quot; + $('user-up').style.display"))); ?></td>
-        </tr>
-      </table>
+      <?php echo toggle('user-hide', 'user-up', 'user-down',
+              $sf_user->getPreference('box_user') == 'none' ? 'down' : 'up',
+              remote_function(array('url' => "@set_preference?pname=box_user", 'with' => "&quot;pvalue=&quot; + $('user-up').style.display"))); ?>
+      <?php echo image_tag('user.gif') ?>
       <div id="user-hide"<?php echo $sf_user->getPreference('box_user') == 'none' ? " style=display:none;" : "" ?>>
         <?php echo __('Welcome') . ' ' . link_to($sf_user->getGuardUser()->getUsername(), 'user/viewProfile?username=' . $sf_user->getGuardUser()->getUsername()) ?>!<br /><br />
         You have <?php echo link_to($user_code_count, 'snippet/listMySnippets') ?> snippet(s).<br /><br />
