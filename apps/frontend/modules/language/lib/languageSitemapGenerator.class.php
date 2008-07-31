33b5d6e5a239f324d99f0262a6e2de374d4e6275
Utku Utkan <utku.utkan@hoydaa.com>
2008-07-31 20:42:24 +0000

    new url for sitemap.xml

--- apps/frontend/modules/language/lib/languageSitemapGenerator.class.php
+++ apps/frontend/modules/language/lib/languageSitemapGenerator.class.php
@@ -4,6 +4,7 @@ class languageSitemapGenerator implements sitemapGenerator
     public static function generate() {
     	$urls = array();
     	$langs = SnippetLanguagePeer::getPopularValidLanguages(500);
+    	$urls[] = new sitemapURL("language/list", date('F j Y'));
     	foreach($langs as $lang) {
     		$urls[] = new sitemapURL("language/".$lang['language'], date('F j, Y'));
     	}
