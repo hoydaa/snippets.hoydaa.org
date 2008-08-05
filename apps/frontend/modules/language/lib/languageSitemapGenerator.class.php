32d97f0209579f10c984c4a8bcb85ff086baf58a
Umut Utkan <umut.utkan@gmail.com>
2008-08-05 19:03:38 +0000

    SNIPPETS-75: Date formats are corrected and frequency, priority fields are added.

--- apps/frontend/modules/language/lib/languageSitemapGenerator.class.php
+++ apps/frontend/modules/language/lib/languageSitemapGenerator.class.php
@@ -4,9 +4,9 @@ class languageSitemapGenerator implements sitemapGenerator
     public static function generate() {
     	$urls = array();
     	$langs = SnippetLanguagePeer::getPopularValidLanguages(500);
-    	$urls[] = new sitemapURL("language/list", date('F j Y'));
+    	$urls[] = new sitemapURL("language/list", date('M j Y'), 'daily', 1.0);
     	foreach($langs as $lang) {
-    		$urls[] = new sitemapURL("language/".$lang['language'], date('F j, Y'));
+    		$urls[] = new sitemapURL("language/".$lang['language'], date('M j, Y'), 'daily', 1.0);
     	}
     	return $urls;
     }
--- apps/frontend/modules/site/lib/siteSitemapGenerator.class.php
+++ apps/frontend/modules/site/lib/siteSitemapGenerator.class.php
@@ -3,7 +3,7 @@ class siteSitemapGenerator implements sitemapGenerator
 {
     public static function generate() {
     	$urls = array();
-    	$urls[] = new sitemapURL("", date('F j, Y'));
+    	$urls[] = new sitemapURL("", date('M j, Y'), 'weekly', 0.8);
     	return $urls;
     }
 }
