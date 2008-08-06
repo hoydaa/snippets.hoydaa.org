c35028c8c4630d4c7b3e784890082a0b50205fee
Umut Utkan <umut.utkan@gmail.com>
2008-08-06 19:46:34 +0000

    SNIPPETS-66: About us text.

--- apps/frontend/modules/site/templates/aboutSuccess.php
+++ apps/frontend/modules/site/templates/aboutSuccess.php
@@ -1,1 +1,26 @@
-About content will be added.
+<?php use_helper('I18N') ?>
+
+<h1><?php echo __('About Snippets') ?></h1>
+
+<p>
+Snippets is a code snippet sharing platform for developers.
+Snippets is developed by the developers of Hoydaa Open Source 
+Organization. Snippets is developed to provide a high quality
+code snippet sharing platform with highlighting, tagging and 
+searching support.
+</p>
+
+<p>
+Snippets is using Hoydaa's open source code highlighting library, 
+CodeSnippet. CodeSnippet is a project under development, every day
+new extensions --support for new languages-- are being added. Snippets
+will take advantage of this improvement by itself. Snippets will also 
+support new languages day by day with no extra user effort. Code snippets
+submitted, will be automatically highlighted when the appropriate 
+support is added to CodeSnippet.
+</p>
+
+<p>
+In the future we are also planning to integrate Snippets with existing blogging
+applications in order to provide developers a single point blogging opportunity.
+</p>
