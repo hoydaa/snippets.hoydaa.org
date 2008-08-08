404ab61de86bbabdf183de79595c4d5d9b0157e9
Umut Utkan <umut.utkan@gmail.com>
2008-08-08 16:52:33 +0000

    SNIPPETS-78, SNIPPETS-79: Contact us and about us are added to the sitemap.

--- apps/frontend/modules/site/lib/siteSitemapGenerator.class.php
+++ apps/frontend/modules/site/lib/siteSitemapGenerator.class.php
@@ -1,9 +1,25 @@
 <?php
 class siteSitemapGenerator implements sitemapGenerator
 {
+
     public static function generate() {
+        $MODULES_DIR = SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . SF_APP.DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR;
+    
     	$urls = array();
     	$urls[] = new sitemapURL("", date('Y-m-d\TH:i:s\Z'), 'weekly', 0.8);
+    	$urls[] = new sitemapURL(
+    	    "content/about", 
+    	    date('Y-m-d\TH:i:s\Z', 
+    	        filemtime($MODULES_DIR.'site'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'aboutSuccess.php')
+    	    ), 
+    	    'monthly', 0.8);
+    	$urls[] = new sitemapURL(
+    	    "content/about", 
+    	    date('Y-m-d\TH:i:s\Z', 
+    	        filemtime($MODULES_DIR.'site'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'contactSuccess.php')
+    	    ), 
+    	    'monthly', 0.8);
     	return $urls;
     }
+
 }
