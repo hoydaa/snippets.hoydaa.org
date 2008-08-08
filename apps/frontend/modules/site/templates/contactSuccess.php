720c6d4fa2fa9f5e867b131c4b9fce0dce726650
Umut Utkan <umut.utkan@gmail.com>
2008-08-08 16:53:00 +0000

    SNIPPETS-79: Static page for contact us.

---
+++ apps/frontend/modules/site/templates/contactSuccess.php
@@ -0,0 +1,5 @@
+<h1>Contact Details</h1>
+
+<p>If you have anything in your mind, anything regarding Snippets, 
+please do not hesitate to send us email. Our email adres is 
+<?php echo mail_to('snippets@hoydaa.org', 'snippets@hoydaa.org'); ?>.</p>
