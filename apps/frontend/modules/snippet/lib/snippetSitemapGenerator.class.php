ac41ac4e47304d083a0c5985e5d12fe4b698143a
Utku Utkan <utku.utkan@hoydaa.com>
2008-08-24 20:22:53 +0000

    SNIPPETS-80: Necessary modifications related with the newly added draft functionality.

--- apps/frontend/modules/snippet/lib/snippetSitemapGenerator.class.php
+++ apps/frontend/modules/snippet/lib/snippetSitemapGenerator.class.php
@@ -1,12 +1,21 @@
 <?php
+
 class snippetSitemapGenerator implements sitemapGenerator
 {
-    public static function generate() {
-    	$urls = array();
-    	$snippets = SnippetPeer::doSelect(new Criteria());
-    	foreach($snippets as $snippet) {
-    		$urls[] = new sitemapURL("snippet/show?id=".$snippet->getId(), date('Y-m-d\TH:i:s\Z', strtotime($snippet->getUpdatedAt())), 'weekly', 1.0);
-    	}
-    	return $urls;
+  public static function generate()
+  {
+    $c = new Criteria();
+    $c->add(SnippetPeer::DRAFT, false);
+
+    $snippets = SnippetPeer::doSelect($c);
+
+    $urls = array();
+
+    foreach($snippets as $snippet)
+    {
+      $urls[] = new sitemapURL("snippet/show?id=".$snippet->getId(), date('Y-m-d\TH:i:s\Z', strtotime($snippet->getUpdatedAt())), 'weekly', 1.0);
     }
+
+    return $urls;
+  }
 }
