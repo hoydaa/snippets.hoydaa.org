b6035dfe938e918b21196c56917b6fefefcbdd1e
Utku Utkan <utku.utkan@hoydaa.com>
2008-08-24 20:20:59 +0000

    SNIPPETS-80: Popup page to describe drafts.

---
+++ apps/frontend/modules/site/templates/_draft.php
@@ -0,0 +1,5 @@
+<h1>Draft</h1>
+<p>Drafts can only be seen by their owners. You can save your post
+as draft by checking this field. When the post is ready you can publish
+it by unchecking the same field.</p>
+<?php echo image_tag('draft.gif') ?>
