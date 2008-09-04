75ebd7e045e62ce7150ddd915de80bd56fea52b4
Umut Utkan <umut.utkan@gmail.com>
2008-09-04 18:37:18 +0000

    SNIPPETS-88

--- apps/frontend/modules/tag/templates/showSuccess.php
+++ apps/frontend/modules/tag/templates/showSuccess.php
@@ -1,5 +1,5 @@
 <?php use_helper('I18N', 'My') ?>
-<?php special_append_to_page_title(__('Languages') . ' : ' . $sf_params->get('tag')); ?>
+<?php special_append_to_page_title(__('Tags') . ' : ' . $sf_params->get('tag')); ?>
 
 <h1><?php echo __('Snippets tagged with "%tag%".', array('%tag%' => $sf_params->get('tag'))) ?></h1>
 
