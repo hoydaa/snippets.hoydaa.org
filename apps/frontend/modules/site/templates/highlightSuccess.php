2946d328720c46e69631bbe84b04ac9e9fe17f4e
Utku Utkan <utku.utkan@hoydaa.com>
2008-09-05 22:40:30 +0000

    Google adsense code is placed.

--- apps/frontend/modules/site/templates/highlightSuccess.php
+++ apps/frontend/modules/site/templates/highlightSuccess.php
@@ -1,1 +1,10 @@
+<div style="float: right;">
+    <script type="text/javascript">
+        google_ad_client = "pub-7151161015858503";
+        google_ad_slot = "5213317032";
+        google_ad_width = 160;
+        google_ad_height = 600;
+    </script>
+    <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+</div>
 <?php echo $code ?>
