52a7e8c69ec1caf3f07e54ca52f08694393329f7
Umut Utkan <umut.utkan@gmail.com>
2009-01-30 20:18:41 +0000

    Added snap.com preview functionality.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -89,6 +89,7 @@
             </div>
         </div>
        
+		<script type="text/javascript" src="http://shots.snap.com/ss/a541f8669d971e4bd67a5bda04d42c8f/snap_shots.js"></script>
         <script type="text/javascript">
             var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
             document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
